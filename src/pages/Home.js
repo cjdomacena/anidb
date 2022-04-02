9af7dbcf0d4d9e2d3fd615f485ab4a04c2e431ce
cjdomacena <cjdomacena@yahoo.com>
2022-04-01 23:53:11 -0400

    fix: :bug: Fix hasNextPage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,49 +9,54 @@ function Home()
 {
 	const { ref, inView } = useInView();
 	const [page, setPage] = useState(0);
-	const [hasNext, setHasNext] = useState(true);
-	const { data, isFetching, fetchNextPage, hasNextPage, isFetchingNextPage } 
-	= useInfiniteQuery('seasonal', ({ pageParam = 0 }) => getCurrentSeason(
-		pageParam
-	), {
-		staleTime: 1000 * 60, refetchOnWindowFocus: false,
-		getPreviousPageParam: firstPage => firstPage.previousId ?? undefined,
-		getNextPageParam: () => page ?? undefined
-	})
+	const { data, isFetching, fetchNextPage, hasNextPage, isFetchingNextPage }
+		= useInfiniteQuery('seasonal', ({ pageParam = 0 }) => getCurrentSeason(
+			pageParam
+		), {
+			staleTime: 1000 * 60, refetchOnWindowFocus: false,
+			getNextPageParam: (lastPage) =>
+			{
+				if (lastPage.pagination.has_next_page)
+				{
+					return page;
+				} else
+				{
+					return undefined;
+				}
+			}
+		})
 
 	useEffect(() =>
 	{
 		if (inView)
 		{
 			setPage(page + 1);
-			if (data?.pages[0].pagination.last_visible_page >= page)
+			if (hasNextPage)
 			{
-
 				fetchNextPage();
-			} else {
-				setHasNext(false)
 			}
-
 		}
 	}, [inView])
 
 
 	return (
-		<section className="container mx-auto mt-12 text-white">
-			<h1 className="text-lg font-bold p-4">Current Season</h1>
-			<div className="w-full grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4">
-				{isFetching && <LoadingCard />}
+		<>
+			<section className="container mx-auto mt-12 text-white">
+				<h1 className="text-lg font-bold p-4">Current Season</h1>
+				<div className="w-full grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4">
+					{isFetching && <LoadingCard />}
 
-				{data && data?.pages.map((page,index )=> (
-					<React.Fragment key={index}>
-						{page.data.map(item => <Card props={item} key={item.mal_id} />)}
-					</React.Fragment>
-				))}
-				{/* {data && data.pages[0].data.map((item) => <Card props={item} key={item.mal_id} />)} */}
-			</div>
-			<div className="p-4 bg-slate-800 my-4 text-center" ref={ref} disabled={!hasNextPage || isFetchingNextPage}>
-				{isFetchingNextPage ? <p>Loading more...</p> : hasNext ? 'Loading' : 'Nothing more to load'}
-			</div>
-		</section>)
+					{data && data?.pages.map((page, index) => (
+						<React.Fragment key={index}>
+							{page.data.map(item => <Card props={item} key={item.mal_id} />)}
+						</React.Fragment>
+					))}
+					{/* {data && data.pages[0].data.map((item) => <Card props={item} key={item.mal_id} />)} */}
+				</div>
+				<div className="p-4 bg-slate-800 my-4 text-center" ref={ref} disabled={!hasNextPage || isFetchingNextPage}>
+					{isFetchingNextPage ? <p>Loading more...</p> : hasNextPage ? 'Loading' : 'Nothing more to load'}
+				</div>
+			</section>
+		</>)
 }
 export default Home;
