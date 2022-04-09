5a96fc663952a3cb8f2a9d951fbaf3acfd13ab6b
cjdomacena <cjdomacena@yahoo.com>
2022-04-09 17:26:39 -0400

    feat: :sparkles: Added links

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,21 @@
 import React, { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { SearchIcon } from '@heroicons/react/solid'
 import { UserContext } from './../../context';
 import LoggedIn from "./LoggedIn";
 
-function Navbar()
+
+function Navbar({setIsOpen})
 {
 	const { session } = useContext(UserContext);
 
 	return (
-		<header className="w-full h-auto bg-slate-900 border-b border-b-slate-800 shadow-sm rounded relative">
+		<header className="w-full h-auto bg-slate-900 border-b border-b-slate-800 shadow-sm rounded relative overflow-hidden">
 			<nav className="container mx-auto flex justify-between h-16 items-center text-white">
 				<h1><Link to="/">AnimeList</Link></h1>
 				<ul className="flex space-x-4 h-full items-center text-base text-gray-200 ">
 					{/* <Browse setToggleBrowse={setToggleBrowse} /> */}
+					<li className='hover:text-slate-600 cursor-pointer' onClick={() => setIsOpen(prev => !prev)}><SearchIcon className="w-6 h-6" /></li>
 					<li className="space-x-4">
 
 						{session === null ? <>
@@ -20,14 +23,15 @@ function Navbar()
 							{/* <Link to="/signup" className="border border-blue-600  px-6 py-2 rounded font-semibold">Signup</Link> */}
 						</> : <LoggedIn />}
 					</li>
+
 				</ul>
 			</nav>
 
-			<div className={`w-full h-auto bg-slate-800 left-0 z-50 p-4`}>
+			<div className={`w-full h-auto bg-slate-800 left-0 z-20 p-4`}>
 				<div className="w-fit mx-auto text-white">
 					<ul className="text-sm flex space-x-4">
-						<li><NavLink to="/" className={({isActive}) => `${ isActive ? "font-bold" : "" }`}>Current Season</NavLink></li>
-						<li><NavLink to="/top-anime" className={({isActive}) => `${ isActive ? "font-bold" : "" }`}>Top Anime</NavLink></li>
+						<li><NavLink to="/" className={({ isActive }) => `${ isActive ? "font-bold" : "" }`}>Current Season</NavLink></li>
+						<li><NavLink to="/top-anime" className={({ isActive }) => `${ isActive ? "font-bold" : "" }`}>Top Anime</NavLink></li>
 						<li>Schedule</li>
 						<li>Random Anime</li>
 					</ul>
